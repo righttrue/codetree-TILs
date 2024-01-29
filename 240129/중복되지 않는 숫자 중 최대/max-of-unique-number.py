cf4fa0922f8413f80412f9677ddc8078e7672de9
righttrue <117806283+righttrue@users.noreply.github.com>
2024-01-29 14:25:14 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 193ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240129/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -0,0 +1,19 @@
+N = int(input())
+
+arr = list(map(int, input().split()))
+
+arr2 = []
+ans = 0
+for i in range(len(arr)):
+    if arr[i] in arr[i+1:]:
+        arr2.append(arr[i])
+max_num = arr[0]
+for i in arr:
+    if i in arr2:
+        continue
+    if max_num <= i:
+        max_num = i
+        ans = 0
+if len(set(arr)) == len(set(arr2)):    print(ans)
+else:
+    print(max_num)
