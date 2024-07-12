78d113be223fef7a76591211f689f8b35df931b4
righttrue <117806283+righttrue@users.noreply.github.com>
2024-07-12 10:30:40 +0900

    Title: 격자 반대로 채우기, Time: 225ms, Memory: 24MB, Status: Passed - CodeTree

--- 240712/격자 반대로 채우기/grid-anti-fill.py
+++ 240712/격자 반대로 채우기/grid-anti-fill.py
@@ -3,12 +3,15 @@ N = n*n
 
 arr_2d = [[0 for _ in range(n)] for _ in range(n)]
 num = 1
+count = 0
 for j in range(n-1, -1,-1):
-    if j%2 != 0:
+    if count%2 == 0:
+        count +=1
         for i in range(n-1, -1,-1):
             arr_2d[i][j] = num
             num +=1
     else:
+        count +=1
         for i in range(n):
             arr_2d[i][j] = num
             num +=1
